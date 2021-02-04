5cfb3096084d285b96d776d746ebfc4adeb323dd
msgarner18 <65476608+msgarner18@users.noreply.github.com>
2021-02-03 22:52:34 -0700

    Update About.js

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -37,7 +37,7 @@ export default class About extends Component {
                         <h3>Members</h3>
                         <div class="teamBio">
                             <div class="inline-block">
-                                <img src="images/mark.png" alt="Profile Picture of Mark Garner" width="180" height="250"/>
+                                <img src="images/mark.jpg" alt="Profile Picture of Mark Garner" width="180" height="250"/>
                                 <div class="bioTxtContainer">
                                     <p><b>Mark Garner</b></p>
                                     <p>Major in Computer Science</p>
