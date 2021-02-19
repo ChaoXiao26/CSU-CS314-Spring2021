7d1ea6bbda19288961d432d8162229070c4c7780
471654852 <s471654852@gmail.com>
2021-02-18 19:38:02 -0700

    Finish up protocol find for today's due

--- server/src/main/java/com/tco/requests/FindRequest.java
+++ server/src/main/java/com/tco/requests/FindRequest.java
@@ -10,7 +10,7 @@ public class FindRequest extends RequestHeader {
     private String match;
     private String[] type, where;
     private Integer limit;
-    private Integer found = 1;
+    private Integer found = 0;
     //private List<Map<String, String>> places;
     ArrayList places = new ArrayList();
 
@@ -31,19 +31,42 @@ public class FindRequest extends RequestHeader {
     @Override
     public void buildResponse() {
 
-        Map<String, String> placeInfo = new HashMap();
-        placeInfo.put("name", "Dave's Airport");
-        placeInfo.put("latitude", "40.0332984924");
-        placeInfo.put("longitude", "-105.124000549");
-        placeInfo.put("id", "0CO1");
-        placeInfo.put("altitude", "5170");
-        placeInfo.put("municipality", "Louisville");
-        placeInfo.put("type", "small_airport");
-        placeInfo.put("region", "Colorado");
-        placeInfo.put("country", "United States");
-        placeInfo.put("url", "https://www.aopa.org/destinations/airports/0CO1/details");
-        places.add(placeInfo);
+        Map<String, String> placeInfo;
+        // = new HashMap();
+        // placeInfo.put("name", "Dave's Airport");
+        // placeInfo.put("latitude", "40.0332984924");
+        // placeInfo.put("longitude", "-105.124000549");
+        // placeInfo.put("id", "0CO1");
+        // placeInfo.put("altitude", "5170");
+        // placeInfo.put("municipality", "Louisville");
+        // placeInfo.put("type", "small_airport");
+        // placeInfo.put("region", "Colorado");
+        // placeInfo.put("country", "United States");
+        // placeInfo.put("url", "https://www.aopa.org/destinations/airports/0CO1/details");
+        // places.add(placeInfo);
+        int lim;
+        try {
+            lim = limit.intValue();
+        }
+        catch (NullPointerException e) {
+            lim = 100;
+        }
+        if(lim == 0){
+            lim = 100;
+        }
 
+        FindDatabase dbt = new FindDatabase(this.match, lim, this.where, this.type);
+        dbt.match(match);
+        ArrayList<String> dbTmpName = new ArrayList();
+        dbTmpName = dbt.Database();
+
+        for (int i = 0; i < dbTmpName.size(); i++) {
+            placeInfo = new HashMap();
+            placeInfo.put("name", dbTmpName.get(i));
+            places.add(placeInfo);
+        }
+        
+        found = dbt.getCount();
         log.trace("buildResponse -> {}", this);
     }
 
