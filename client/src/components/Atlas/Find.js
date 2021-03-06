6bdb0f9619d0cbaf56f0a1704532eeb81a4bd974
Tomas Vasquez <vasquezt@colostate.edu>
2021-03-06 14:31:21 -0700

    Create Pop up and table header

--- client/src/components/Atlas/Find.js
+++ client/src/components/Atlas/Find.js
@@ -2,6 +2,7 @@ import React, {Component, useEffect} from 'react';
 import {Button, InputGroup, InputGroupAddon, InputGroupText, Input, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 import 'leaflet/dist/leaflet.css';
 import { sendServerRequest, isJsonResponseValid, getOriginalServerPort} from "../../utils/restfulAPI";
+import Coordinates from "coordinate-parser";
 import * as findSchema from "../../../schemas/FindResponse";
 
 const SerPort = getOriginalServerPort()
@@ -18,13 +19,15 @@ export default class Find extends Component {
             modalNew: false,
             modalFindResponse: false,
             validServer: null,
-            find: {}
+            find: {},
+            //foundLocations: []
         }
     }
     render() {
         return ( 
             <div>
             {this.renderFindInput()}
+            {this.renderFindResponse()}
             </div>
 
         );
@@ -100,6 +103,62 @@ export default class Find extends Component {
             modalNew: !this.state.modalNew
         });
     }
+    renderFindResponse(){
+        return (
+            <div>
+            <Modal isOpen={this.state.modalFindResponse} toggle={this.findResponseToggle}>
+                <ModalHeader toggle={this.findResponseToggle}>Places Found</ModalHeader>
+                <ModalBody>
+                    <InputGroup>
+                    {this.renderFindTableResponse()}
+                    </InputGroup>
+                </ModalBody>
+            </Modal>
+        </div>
+        );
+       
+    }
+    createLocationTable(){
+        const foundLocations = find.places.map((location,i) =>
+            <tr key={i+=1}>
+                <th>{location.altitude}</th>
+                <th>{location.country}</th> 
+            </tr>
+            );
+            return foundLocations;
+
+    }
+    renderFindTableResponse(){
+        //const foundLocations = this.state.find.places.names;
+        /* const foundLocations = this.state.find.places.map((location,i) =>
+            <tr key={i+=1}>
+                <th>{location.altitude}</th>
+                <th>{location.country}</th> 
+            </tr>
+            ); */
+        //this.createLocationTable();  
+        return(
+            <table className="table table-striped table-bordered table-sm">
+                <thead>
+                    <tr> 
+                        <th className="smallCell"><b>#</b></th>
+                        <th><b>altitude</b></th>
+                        <th><b>country</b></th>
+                        <th><b>id</b></th>
+                        <th><b>Latitude</b></th>
+                        <th><b>Longitude</b></th> 
+                        <th><b>municipality</b></th>
+                        <th><b>name</b></th>
+                        <th><b>region</b></th>
+                        <th><b>type</b></th>
+			            <th className="smallCell"><Button color="primary" type="button" className="btn btn-secondary btn-block float-right" onClick={this.clearTable}>Clear</Button></th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        );
+    }
     findResponseToggle() {
         this.setState({
             modalFindResponse: !this.state.modalFindResponse
@@ -117,7 +176,7 @@ export default class Find extends Component {
         } else {
             this.setState({validServer: true, find: findResponse});
             this.findResponseToggle();
-            //console.log(this.state.modalFindResponse);
+            //console.log(this.state.find.places);
         }
     }
 
