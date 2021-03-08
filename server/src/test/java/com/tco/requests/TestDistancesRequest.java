4f43cef3e58c3cb6d0dec3218fc79f309dc947fc
sbonafe <60677766+sbonafe@users.noreply.github.com>
2021-03-08 13:14:18 -0700

    Create TestDistancesRequest.java

---
+++ server/src/test/java/com/tco/requests/TestDistancesRequest.java
@@ -0,0 +1,26 @@
+package com.tco.requests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestDistancesRequest {
+  
+  private DistanceRequest distanceRequest;
+  
+  @BeforeEach
+  public void createConfigurationForTestCases(){
+    distanceRequest = new DistanceRequest();
+    distanceRequest.buildResponse();
+  }
+  
+  @Test
+  @DisplayName(("Request type is \"distances\"")
+  public void testType(){
+    String type = distanceRequest.getRequestType();
+    assertEquals("distances", type);
+  }
+}
