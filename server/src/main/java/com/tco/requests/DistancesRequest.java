d2291f0c33427a9e7650e83d9216ff8db99cfc4c
sbonafe <60677766+sbonafe@users.noreply.github.com>
2021-03-08 12:49:24 -0700

    Update DistancesRequest.java
    Add DistanceRequest.java

--- server/src/main/java/com/tco/requests/DistancesRequest.java
+++ server/src/main/java/com/tco/requests/DistancesRequest.java
@@ -4,6 +4,24 @@ import com.tco.requests.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.*;
+
 public class DistancesRequest extends RequestHeader {
+    ArrayList<Map<String, String>> places = new ArrayList<Map<String, String>>();
+    private float earthRadius;
+    private int[] distances;
     
+    public DistancesRequest(ArrayList<Map<String, String>> places, float earthRadius, int[] distances){
+        this.requestType = "distances";
+        this.places = places;
+        this.earthRadius = earthRadius;
+        this.distances = distances;
+    }
+
+    private final transient Logger log = LoggerFactory.getLogger(DistancesRequest.class);
+    
+    @Override
+    public void buildResponse(){
+       // log.trac("buildResponse -> {}", this);
+    }
 }
