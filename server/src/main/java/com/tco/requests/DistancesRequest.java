e397ffde23977cf8098a5d24009f2cce0508f1ba
Fan6 <s471654852@gmail.com>
2021-03-10 18:53:49 +0000

    Add greatCircle function & a few test cases

--- server/src/main/java/com/tco/requests/DistancesRequest.java
+++ server/src/main/java/com/tco/requests/DistancesRequest.java
@@ -20,8 +20,34 @@ public class DistancesRequest extends RequestHeader {
 
     private final transient Logger log = LoggerFactory.getLogger(DistancesRequest.class);
     
+    public double greatCircle(double lat1, double lng1, double lat2, double lng2, double earthRad){
+        if ((lat1 != lat2) && (lng1 != lng2)){
+
+			double dLat = lng1 - lng2;
+            double dLng = Math.toRadians(lng2-lng1);
+
+			double a = Math.sin(dLat/2) * Math.sin(dLat/2)
+                     + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                     * Math.sin(dLng/2) * Math.sin(dLng/2);
+            double angle = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+			double dist = 0;
+            dist = angle * earthRad;
+
+			return (dist);
+		}
+
+        else{
+            return 0;
+        }
+    
+    }
+
     @Override
     public void buildResponse(){
+        distances.add((int)greatCircle(40.6, -105.1, -33.9, 151.2, 3959));      //a few test cases
+        distances.add((int)greatCircle(40.6, -105.1, -33.9, 151.2, 6371));
+        distances.add((int)greatCircle(0, 0, 0, 0, 0));
         log.trace("buildResponse -> {}", this);
     }
     
