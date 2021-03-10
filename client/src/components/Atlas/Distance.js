5ac8939223eff7c7b5cf8070a5e72e634fe17eba
Yuxin Huang <huangyuxin1226@icloud.com>
2021-03-10 06:11:56 +0000

    User needs to be presented with a window that tells them to pick two places in the map

--- client/src/components/Atlas/Distance.js
+++ client/src/components/Atlas/Distance.js
@@ -7,14 +7,55 @@ export default class Distance extends Component {
     constructor(props) {
         super(props);
         this.props = props;
+        this.DistanceToggle = this.DistanceToggle.bind(this);
         this.state = {
+            modalDistance: false
         }
     }
     render() {
         return ( 
             <div>
-                <Button className="my-1" onClick ={this.requestUserLocation} color = "primary" block>Distance</Button>
+                <Button className="my-1" onClick ={this.DistanceToggle} color = "primary" block>Distance</Button>
+                {this.DistancePopup()}
             </div>
+            
         );
+    }
+
+    DistancePopup(){
+        return(
+            <div>
+                <Modal isOpen={this.state.modalDistance} toggle={this.DistanceToggle}>
+                    <ModalHeader toggle={this.DistanceToggle}>Calculate Distance</ModalHeader>
+                    <ModalBody>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">
+                                <InputGroupText>First Coordinate</InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                                placeholder="latitude, longitude"
+                            />
+                        </InputGroup>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">
+                                <InputGroupText>Second Coordinate</InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                                placeholder="latitude, longitude"
+                            />
+                        </InputGroup>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color='secondary'>Calculate</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        )
+    }
+
+    DistanceToggle(){
+        this.setState({
+            modalDistance: !this.state.modalDistance
+        });
     }
 }
