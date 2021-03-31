e180ce04da693f60c7d5f869c165af73c61579fa
Fan6 <s471654852@gmail.com>
2021-03-31 03:38:16 +0000

    create TestTourRequest.java

---
+++ server/src/test/java/com/tco/requests/TestTourRequest.java
@@ -0,0 +1,12 @@
+package com.tco.requests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestTourRequest {
+    //private TourRequest tour;
+}
