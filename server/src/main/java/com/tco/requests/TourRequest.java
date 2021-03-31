96809957c986ea04d2b0b53afb3a283b756a5aba
Yuxin Huang <huangyuxin1226@icloud.com>
2021-03-31 03:36:56 +0000

    Create TourRequest.java

---
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -0,0 +1,24 @@
+package com.tco.requests;
+import com.tco.requests.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+public class TourRequest extends RequestHeader {
+    ArrayList<Map<String, String>> places;
+    private float earthRadius;
+    private double response;
+
+    public TourRequest(float earthRadius, double response, ArrayList<Map<String, String>> places){
+        this.requestType = "tour";
+        this.earthRadius = earthRadius;
+        this.response = response;
+        this.places = places;
+    }
+    @Override
+    public void buildResponse() {
+        
+    }
+}
