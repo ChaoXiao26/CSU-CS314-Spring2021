340d9ffb1503e1a8b63f0fac9e3d92f192f91f7f
Tomas Vasquez <77645615+vasquezt15@users.noreply.github.com>
2021-04-04 19:29:41 -0600

    Update Atlas.js
    Delete download function thats not needed anymore

--- client/src/components/Atlas/Atlas.js
+++ client/src/components/Atlas/Atlas.js
@@ -420,17 +420,5 @@ export default class Atlas extends Component {
             
         }
     }
-    downloadFile(fileText, fileName, fileType) {
-        let file = new Blob([JSON.stringify(fileText)], {type: fileType});
-        let a = document.createElement('a'),
-        url = URL.createObjectURL(file);
-        a.href = url;
-        a.download = fileName;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(function() {
-          document.body.removeChild(a);
-          window.URL.revokeObjectURL(url);
-        }, 0);
-      }
+    
 }
