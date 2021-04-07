29fa010cc5b169719cbd6223b5cf2bc417edca21
Yuxin Huang <huangyuxin1226@icloud.com>
2021-04-07 23:31:36 +0000

    handle both JSON and CSV files

--- client/src/components/Atlas/Load.js
+++ client/src/components/Atlas/Load.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { Button, InputGroup, InputGroupAddon, InputGroupText, Input, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, InputGroup, InputGroupAddon, InputGroupText, Input, Modal, ModalHeader, ModalBody, ModalFooter, ToastHeader } from 'reactstrap';
 import 'leaflet/dist/leaflet.css';
 import Coordinates from "coordinate-parser";
+import { canvas } from 'leaflet';
 
 export default class Load extends Component {
     constructor(props) {
         super(props);
         this.props = props;
         this.handlefile = this.handlefile.bind(this);
+        this.anaylyzeFile = this.anaylyzeFile.bind(this);
+        this.csvFile = this.csvFile.bind(this);
+        this.jsonFile = this.jsonFile.bind(this);
         this.state = {
             uplodedFile: null
         }
@@ -18,8 +22,7 @@ export default class Load extends Component {
             <div>
                 <b>Trip File Upload</b><br></br>
                 <input type="file" id = 'input' multiple onClick ={() => this.handlefile()}/>
-                <Button type="submit" onClick ={() => this.handlefile()} >Upload</Button>
-                {/* <pre id="fileDisplayArea"></pre>              */}
+                {/* <Button type="submit" onClick ={() => this.handlefile()} >Upload</Button> */}
             </div>
             
         );
@@ -28,18 +31,30 @@ export default class Load extends Component {
     handlefile(){
         var ths = this;
         var fileInput = document.getElementById('input');
-        //var fileDisplayArea = document.getElementById('fileDisplayArea');
         fileInput.addEventListener('change', function(e) {
 			var file = fileInput.files[0];
             var reader = new FileReader();
-            reader.onload = function(e) {
-                //fileDisplayArea.innerText = reader.result;
-                console.log(e.target.result);  
+            reader.onload = function(e) {  
                 ths.setState({uplodedFile: e.target.result})
-                console.log(ths.state.uplodedFile);  
+                ths.anaylyzeFile(ths.state.uplodedFile, file.type);
             }
             reader.readAsText(file); 
         });
+    }
 
+    anaylyzeFile(data, type){
+        switch(type){
+            case "application/vnd.ms-excel": this.csvFile(data); break;
+            case "application/json": this.jsonFile(data); break;
+            default: break;
+        }
+    }
+
+    csvFile(data){
+        console.log("csv")
+    }
+
+    jsonFile(data){
+        console.log("json")
     }
 }
